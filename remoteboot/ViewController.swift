7488999d06a320e546e3725a2e24cc42bd715950
TaurusXin <457375479@qq.com>
2019-08-21 12:30:45 +0800

    Initial Commit

---
+++ remoteboot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  remoteboot
+//
+//  Created by 张宇欣 on 2019/8/21.
+//  Copyright © 2019 rhyland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
